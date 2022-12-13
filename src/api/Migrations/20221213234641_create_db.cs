20b170708f7318dd3a3ddab6b4666f418d159394
harmelson <gabriel.harmel@hotmail.com>
2022-12-13 20:59:12 -0300

    build(migrations): create db migrations

---
+++ src/api/Migrations/20221213234641_create_db.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class createdb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Lead",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CPF = table.Column<int>(type: "int", maxLength: 11, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(58)", maxLength: 58, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lead", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Registration",
+                columns: table => new
+                {
+                    IdLead = table.Column<int>(type: "int", nullable: false),
+                    IdCourse = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Registration", x => new { x.IdLead, x.IdCourse });
+                    table.UniqueConstraint("AK_Registration_IdCourse", x => x.IdCourse);
+                    table.ForeignKey(
+                        name: "FK_Registration_Lead_IdLead",
+                        column: x => x.IdLead,
+                        principalTable: "Lead",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Course",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(58)", maxLength: 58, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Course", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Course_Registration_Id",
+                        column: x => x.Id,
+                        principalTable: "Registration",
+                        principalColumn: "IdCourse",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Course");
+
+            migrationBuilder.DropTable(
+                name: "Registration");
+
+            migrationBuilder.DropTable(
+                name: "Lead");
+        }
+    }
+}
