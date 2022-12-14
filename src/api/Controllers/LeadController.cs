41425475e075c500ea5d9feff261ac3a17281746
harmelson <gabriel.harmel@hotmail.com>
2022-12-13 23:27:13 -0300

    feat(controller): add leadController

---
+++ src/api/Controllers/LeadController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+using Api.Repository;
+using Api.Models;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("lead")]
+    public class LeadController : Controller
+    {
+        private readonly LeadRepository _repository;
+        public LeadController(LeadRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Add([FromBody] Lead lead)
+        {
+            return Created("",_repository.AddLead(lead));
+        }
+
+        // [HttpGet]
+        // [AllowAnonymous]
+        // public ActionResult Get()
+        // {
+        //     Console.WriteLine("passou controller");
+        //     return Ok();
+        // }
+    }
+}
+    
